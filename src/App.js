fd4115328d593c7fd1b107aa976486837cd1998b
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-03-28 18:41:37 +0300

    create basic game structure with css styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from "react";
 import './App.css';
+import Board from "./components/Board.jsx";
+import Keyboard from "./components/Keyboard";
+import { boardDeafault } from "./Words";
+
+export const AppContext = createContext()
 
 function App() {
+  const [board, setBoard] = useState(boardDeafault);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Wordle</h1>
+        <AppContext.Provider value={{board, setBoard}}>
+          <Board />
+          <Keyboard />
+        </AppContext.Provider>
+      </nav>
     </div>
   );
 }
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Key from "./Key.jsx";
+
+const Keyboard = () => {
+    const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+    const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+    const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+    return(
+        <div className="keyboard">
+            <div className="line">
+                {keys1.map(key=><Key keyVal={key} key={key}/>)}
+            </div>
+            <div className="line">
+                {keys1.map(key=><Key keyVal={key} key={key}/>)}
+            </div>
+            <div className="line">
+                <Key keyVal={"ENTER"} bigKey/>
+                {keys1.map(key=><Key keyVal={key} key={key}/>)}
+                <Key keyVal={"DELETE"} bigKey/>
+            </div>
+        </div>
+    )
+}
+
+export default Keyboard;
